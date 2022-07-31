5442e95dec0b1681056156408d39d1c6e7c819f3
karma.2710 <95325806+s3916890@users.noreply.github.com>
2022-07-31 20:28:37 +0700

    .

--- Main.java
+++ Main.java
@@ -1,9 +1,6 @@
-import app.lib.crud.read.ReadAlline;
 import app.view.menu.Menu;
 import app.view.order.product.Product;
-
 import java.io.*;
-import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
