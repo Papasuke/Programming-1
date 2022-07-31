e6f91795696c30c96255c1bc33ab06ce87abf79a
karma.2710 <95325806+s3916890@users.noreply.github.com>
2022-07-31 23:23:36 +0700

    .

--- app/view/order/product/Product.java
+++ app/view/order/product/Product.java
@@ -1,11 +1,6 @@
 package app.view.order.product;
 
 import app.lib.crud.read.ReadAlline;
-import app.lib.crud.read.ReadSpecificColumn;
-
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 
